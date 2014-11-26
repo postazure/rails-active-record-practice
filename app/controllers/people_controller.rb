e0d1270b3bdb16e0f8b6138858e9129c9f63d5c6
postazure <gordonmaxc@gmail.com>
2014-11-26 10:51:00 -0800

    Completed

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,6 +2,9 @@ class PeopleController < ApplicationController
 
   def index
     @people = Person.all
+    @people = @people.order(:last_name)
+    @people = @people.where(awesome: true, eye_color: "Blue")
+
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Person.create(first_name: "Karren", last_name: "Jones", eye_color: "Blue", date_of_birth: "12/12/2012", height_in_inches: 26, awesome: false)
+# Person.create(first_name: "Paul", last_name: "Jones", eye_color: "Green", date_of_birth: "12/12/2012", height_in_inches: 26, awesome: false)
+# Person.create(first_name: "Brian", last_name: "Bardson", eye_color: "Green", date_of_birth: "12/12/2012", height_in_inches: 26, awesome: false)
+# Person.create({first_name: "Kyle", last_name: "Xy", eye_color: "Brown", date_of_birth: "12/12/2012", height_in_inches: 26, awesome: false})
+# Person.create({first_name: "Thomas", last_name: "Gunn", eye_color: "Brown", date_of_birth: "12/12/2012", height_in_inches: 26, awesome: false})
+# Person.create({first_name: "Patrick", last_name: "Smith", eye_color: "Green", date_of_birth: "12/12/2012", height_in_inches: 26, awesome: false})
+# Person.create(:first_name => "Tommy", :last_name => "Boy", :eye_color => "Red", :date_of_birth => "4/2/1990", :height_in_inches => 56, :awesome => true)
+# Person.create(:first_name => "Garth", :last_name => "Brooks", :eye_color => "Blue", :date_of_birth => "4/2/1990", :height_in_inches => 56, :awesome => true)
+# Person.create(:first_name => "Bart", :last_name => "Simpson", :eye_color => "Blue", :date_of_birth => "4/2/1990", :height_in_inches => 56, :awesome => true)
